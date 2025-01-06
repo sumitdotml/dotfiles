e336cb84073c99747c3b08c6f7eeb32ad0376fe1
sumitdotml <sumit@sumit.ml>
2025-01-06 23:13:04 +0700

    basic lspconfig and none-ls

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -9,9 +9,7 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls"
-				}
+				ensure_installed = { "lua_ls", "ts_ls"}
 			})
 		end
 	},
@@ -19,8 +17,12 @@ return {
 		"neovim/nvim-lspconfig",
 		config = function()
 			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
-			vim.keymap.set('n', 'K', vim.lsp.buf.hover, {}) -- hover keymap
+			lspconfig.lua_ls.setup({}) -- for lua
+			lspconfig.ts_ls.setup({}) -- for js, ts
+
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
 		end
 	}
 }
