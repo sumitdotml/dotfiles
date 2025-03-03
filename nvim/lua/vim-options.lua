084878c8c72183669b7885760ac3515e9ba4e74a
sumit <sumit@sumit.ml>
2025-03-03 16:26:33 +0900

    Update vim-options.lua

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -3,4 +3,5 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
-vim.wo.number = true
+-- vim.wo.number = true -- for absolute line numbering
+vim.wo.relativenumber = true -- for relative line numbering
