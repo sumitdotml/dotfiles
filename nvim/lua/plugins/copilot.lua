e2c2ba755e74a42f157fd675465f849c76045367
sumitdotml <sumit@sumit.ml>
2025-05-31 09:30:27 +0900

    added keybinding to enable/disable copilot

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -4,4 +4,15 @@
 return {
 	"github/copilot.vim",
 	lazy = false,
+	enabled = true,
+	-- enable or diable copilot with keymap
+	vim.keymap.set("n", "<leader>cp", function()
+		if vim.g.copilot_enabled then
+			vim.g.copilot_enabled = false
+			vim.notify("Copilot disabled")
+		else
+			vim.g.copilot_enabled = true
+			vim.notify("Copilot enabled")
+		end
+	end, { desc = "Toggle Copilot" })
 }
