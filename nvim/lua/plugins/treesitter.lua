1f726169ac4795b658b47e57aab2cd54c97cee3e
sumit <179308157+sumitdotml@users.noreply.github.com>
2026-04-17 11:04:08 +0900

    pinned nvim-treesitter to master branch for plugin compatibility

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,15 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	branch = "master",
 	build = ":TSUpdate",
 	opts = {
 		ensure_installed = { "markdown", "markdown_inline", "tsx", "typescript" },
 		auto_install = true,
+		highlight = { enable = true },
+		indent = { enable = true },
 	},
 	config = function(_, opts)
 		vim.treesitter.language.register("tsx", "mdx")
-		require("nvim-treesitter").setup(opts)
+		require("nvim-treesitter.configs").setup(opts)
 	end,
 }
